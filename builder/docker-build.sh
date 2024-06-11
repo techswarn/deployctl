f07f7d1ef6d6ef7f331135610a961cfa92c4943f
techswarn <tech.swarns@gmail.com>
2024-06-11 22:16:05 +0530

    code update

--- builder/docker-build.sh
+++ builder/docker-build.sh
@@ -1,12 +1,12 @@
 #build
-docker build -f Dockerfile.build -t swarntech/appship-build:ubuntu-22 .
-docker build -f Dockerfile.run -t swarntech/appship-run:ubuntu-22 .
+docker build --platform="linux/amd64" --no-cache -f Dockerfile.build -t swarntech/appship-build:ubuntu-22 .
+docker build --platform="linux/amd64" --no-cache -f Dockerfile.run -t swarntech/appship-run:ubuntu-22 .
 pack builder create swarntech/appship-buildpacks:ubuntu-22 --config ./builder.toml
 docker tag swarntech/appship-buildpacks:ubuntu-22 swarntech/appship-buildpacks:latest
 docker push swarntech/appship-buildpacks:ubuntu-22
-docker build -f Dockerfile.builder -t swarntech/appship-builder:latest .
+docker build --platform="linux/amd64" --no-cache -f Dockerfile.builder -t swarntech/appship-builder:latest .
 
 #publish
 docker push swarntech/appship-build:ubuntu-22
 docker push swarntech/appship-run:ubuntu-22
-docker push swarntech/appship-builder:latest 
+docker push swarntech/appship-builder:latest
